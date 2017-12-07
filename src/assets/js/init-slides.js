309e2061e97c37c2c34f87066f365519dab51961
Ken Hoff <ken@hoff.tech>
2017-12-07 14:02:32 -0700

    working on waveform talk

--- src/assets/js/init-slides.js
+++ src/assets/js/init-slides.js
@@ -1,2 +1,10 @@
-$.deck(".slide")
+$.deck(".slide", {
+    hashPrefix: '',
+})
 hljs.initHighlightingOnLoad();
+$(".location").text(window.location).prop("href", window.location)
+
+$(document).bind("deck.change", function(event, from, to) {
+    newURL = [window.location.href.split("#")[0], to].join("#")
+    $(".location").text(newURL).prop("href", newURL)
+})
