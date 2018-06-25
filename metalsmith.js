3d73028c3c41dbf0454444b114a48f2f43d15150
Ken Hoff <ken@hoff.tech>
2018-06-25 08:44:45 -0600

    updating config to support posts + other misc bits

--- metalsmith.js
+++ metalsmith.js
@@ -2,12 +2,13 @@ const Metalsmith = require("metalsmith");
 const pug = require("metalsmith-pug");
 const changed = require("metalsmith-changed");
 const sass = require("metalsmith-sass");
+const jstransformer = require("metalsmith-jstransformer");
+const branch = require("metalsmith-branch");
 
 Metalsmith(__dirname)
     .clean(false)
     .use(changed())
-    .source("./src")
-    .destination("./build")
+    .use(branch().pattern(["posts/**/*.md", "layouts/**",]).use(jstransformer({defaultLayout: "layouts/post.pug"})))
     .use(pug())
     .use(sass())
     .build((err) => {
