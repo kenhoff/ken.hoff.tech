d2ad52ba27909b328394b3ca40173905a1ebc152
Ken Hoff <ken@hoff.tech>
2018-08-15 09:26:43 -0600

    wip from stream

--- metalsmith.js
+++ metalsmith.js
@@ -1,17 +1,32 @@
-const Metalsmith = require("metalsmith");
-const pug = require("metalsmith-pug");
-const changed = require("metalsmith-changed");
-const sass = require("metalsmith-sass");
+const Metalsmith = require('metalsmith');
+const pug = require('metalsmith-pug');
+const changed = require('metalsmith-changed');
+const sass = require('metalsmith-sass');
+const markdown = require('metalsmith-markdown');
+const layouts = require('metalsmith-layouts');
+const branch = require('metalsmith-branch');
 
 Metalsmith(__dirname)
     .clean(false)
     .use(changed())
-    .source("./src")
-    .destination("./build")
+    .source('./src')
+    .destination('./build')
+    // .use(markdown())
     .use(pug())
     .use(sass())
-    .build((err) => {
+    .use(
+        branch()
+            .pattern(['posts/**/*.md'])
+            .use(
+                layouts({
+                    engine: 'pug',
+                    default: 'post.pug',
+                    directory: 'layouts',
+                }),
+            ),
+    )
+    .build(err => {
         if (err) {
             console.log(err);
         }
-    })
+    });
