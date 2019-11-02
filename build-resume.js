6a84bb8671ae12dd44e86556e76b214b6882aacd
Ken Hoff <ken@hoff.tech>
2019-11-02 15:54:36 -0600

    build resume scripts

---
+++ build-resume.js
@@ -0,0 +1,17 @@
+const puppeteer = require("puppeteer");
+const dayjs = require("dayjs");
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("file:///Users/ken/dev/ken.hoff.tech/resume/resume.html", {
+        waitUntil: "networkidle2"
+    });
+
+    await page.pdf({
+        path: `build/Ken-Hoff-Resume-${dayjs().format("YYYY-MM-DD")}.pdf`,
+        printBackground: true
+    });
+
+    await browser.close();
+})();
